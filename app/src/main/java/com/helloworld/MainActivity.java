aa33a1705473faba925b4c150c229a8fe0b6c5a5
joontai98 <joontae98@gmail.com>
2020-10-04 16:37:22 +0900

    Add: Add addTextChangedListener

--- app/src/main/java/com/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.RelativeLayout;
 import com.google.android.material.textfield.TextInputEditText;
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     TextInputEditText TextInputEditText_email, TextInputEditText_password;
     RelativeLayout RelativeLayout_login, RelativeLayout_youtube;
+
+    String emailOk = "1234@gmail.com";
+    String passwordOk = "1234";
+    String email = "";
+    String password = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +30,50 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout_login        = findViewById(R.id.RelativeLayout_login);
         RelativeLayout_youtube      = findViewById(R.id.RelativeLayout_youtube);
 
-        //1. 값을 가져온다
+        //1. 값을 가져온다 - 검사 (1234@gmail.com / 1234)
         //2. 클릭을 감지한다
         //3. 1번의 값을 다른 액티비티로 넘긴다
 
-        RelativeLayout_login.setClickable(true);
+        RelativeLayout_login.setEnabled(false);
+        TextInputEditText_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s != null){
+                    email = s.toString();
+                    RelativeLayout_login.setEnabled(validation());
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        TextInputEditText_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s != null){
+                    password = s.toString();
+                    RelativeLayout_login.setEnabled(validation());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         RelativeLayout_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,5 +87,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    public boolean validation() {
+        return email.equals(emailOk) && password.equals(passwordOk);
     }
 }
